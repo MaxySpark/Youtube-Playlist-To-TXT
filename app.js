4aa1f55bf7039782edeb8989b69392a7056ad91a
MaxySpark <maxyspark@gmail.com>
2017-02-05 21:05:19 +0530

    update app and added views

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const engines = require('consolidate');
+const app = express();
+const fs = require('fs');
+var PORT = process.env.PORT || 3000;
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + "/views");
+app.use(express.static('img'));
+var images = 0;
+var imgarr =[];
+app.get('/',function(req, res){
+    res.render('index');
+});
+app.listen(PORT, function() {
+    console.log("Server running at port"+ PORT+"!!!");
+});
